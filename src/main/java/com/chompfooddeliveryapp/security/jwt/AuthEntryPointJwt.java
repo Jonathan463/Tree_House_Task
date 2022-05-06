c2b78b6979ab165ba983f2f48adf4a10ccee8715
Elvis Ikenna <elvisikenna07@gmail.com>
2022-05-06 11:00:48 +0100

    chomp app

--- src/main/java/com/chompfooddeliveryapp/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/chompfooddeliveryapp/security/jwt/AuthEntryPointJwt.java
@@ -17,6 +17,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
         log.error("Unauthorized error: {}", authException.getMessage());
+
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Error: Unauthorized");
 
     }
